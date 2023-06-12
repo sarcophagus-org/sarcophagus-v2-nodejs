9c5751ce73e00a8d512082b4579f05e38333da76
knoll3 <kyle.noll@decentlabs.io>
2023-06-12 16:08:14 -0500

    Temp

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import { NodeSarcoClient } from "sarcophagus-v2-sdk/node";
+dotenv.config();
+
+const privateKey = process.env.PRIVATE_KEY ?? "";
+const providerUrl = process.env.PROVIDER_URL ?? "";
+console.log("🚀 ~ file: index.ts:7 ~ providerUrl:", providerUrl);
+
+const sarco = new NodeSarcoClient({ privateKey, providerUrl });
+
+// console.log(sarco);
