c4799e4ae8658829577e82aa428605702b8b42e0
Seth Hrbek <seth.hrbek@gmail.com>
2023-06-12 21:08:48 -0400

    setup tsconfig

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+import { NodeSarcoClient } from "sarcophagus-v2-sdk";
+const privateKey = process.env.PRIVATE_KEY ?? "";
+const providerUrl = process.env.PROVIDER_URL ?? "";
+console.log("🚀 ~ file: index.ts:7 ~ providerUrl:", providerUrl);
+const sarco = new NodeSarcoClient({ privateKey, providerUrl });
+// console.log(sarco);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import dotenv from "dotenv";
-import { NodeSarcoClient } from "sarcophagus-v2-sdk";
-dotenv.config();
+import 'dotenv/config'
+import { SarcophagusState, WebSarcoClient, NodeSarcoClient } from "sarcophagus-v2-sdk";
 
 const privateKey = process.env.PRIVATE_KEY ?? "";
 const providerUrl = process.env.PROVIDER_URL ?? "";
 console.log("🚀 ~ file: index.ts:7 ~ providerUrl:", providerUrl);
 
-const sarco = new NodeSarcoClient({ privateKey, providerUrl });
+console.log(WebSarcoClient)
+
+// const sarco = new NodeSarcoClient({ privateKey, providerUrl });
 
 // console.log(sarco);
