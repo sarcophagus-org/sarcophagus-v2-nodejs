e39877214ed2d7b41daa7f1260f8f53e1c0a0f18
knoll3 <kyle.noll@decentlabs.io>
2023-06-20 12:11:52 -0500

    Create sarcophagus working

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,193 @@
 import "dotenv/config";
-import { ArchaeologistProfile, ChunkingUploader, NodeSarcoClient } from "sarcophagus-v2-sdk";
 import { BigNumber, ethers } from "ethers";
+import {
+  ArchaeologistNegotiationResult,
+  ChunkingUploader,
+  NodeSarcoClient,
+} from "sarcophagus-v2-sdk";
 
+// Set up environment variables
+// Be sure to define these in your .env
 const privateKey = process.env.PRIVATE_KEY ?? "";
 const providerUrl = process.env.PROVIDER_URL ?? "";
 const chainId = parseInt(process.env.CHAIN_ID) ?? 5;
 const etherscanApiKey = process.env.ETHERSCAN_API_KEY ?? "";
 
 async function main() {
-  const sarco = new NodeSarcoClient({ privateKey, providerUrl, chainId, etherscanApiKey });
-  await sarco.init();
-  const archsData = await sarco.archaeologist.getFullArchProfiles(undefined, true);
-  const archs = archsData.slice(0, 5);
+  // Define script parameters
+  const negotiationAttemptCount = 5;
+  const negotiationAttemptTimeout = 2000;
 
+  // Define sarcophagus parameters
+  const sarcophagusName = "SDK Sarcophagus"; // the name of the sarcophagus
+  const fileName = "hello_world.txt"; // the name of the file to upload
+  const fileContents = "Hello World!"; // the contents of the file to upload
+  const archaeologistAddresses = [
+    "0x7cff7c3c1f150a59a202cf5ec1bb8b57908e385e",
+    "0x8a8a2d79ecab5705697b44cc20e97299a53ca86e",
+    "0x2f3be3c471fe133c47c1f2a805b65b7f38eaa5eb",
+  ]; // the addresses of the archaeologists to use
+  const requiredArchaeologistCount = 2; // the number of archaeologists required to resurrect
+  const resurrectionTime = Date.now() + 1000 * 60 * 60 * 24 * 7; // the resurrection time in milliseconds
+
+  // Create random wallets for the payload and recipient just for this example
+  const payloadWallet = ethers.Wallet.createRandom();
+  const recipientWallet = ethers.Wallet.createRandom();
+
+  // 1. Initialize SDK
+  console.log("\n1. Initialize SDK");
+  console.log("Initializing SDK...");
+  const sarco = new NodeSarcoClient({ privateKey, providerUrl, chainId, etherscanApiKey });
+  await sarco.init().catch((error) => {
+    console.error("Failed to initialize SDK");
+    throw error;
+  });
+
+  // 2. Get archaeologist profiles
+  console.log("\n2. Get Archaeologist Profiles");
+  console.log("Getting archaeologist profiles...");
+  const archaeologists = await sarco.archaeologist
+    .getFullArchProfiles(archaeologistAddresses, true)
+    .catch((error) => {
+      console.error("Failed to get archaeologist profiles");
+      throw error;
+    });
+  console.log(`Retrieved ${archaeologists.length} archaeologist profiles:`);
+  archaeologists.forEach((arch, i) => {
+    console.log(`  ${i + 1}. ${arch.profile.archAddress}`);
+  });
+
+  // 3. Dial and connect to archaeologists
+  console.log("\n3. Dial Archaeologists");
+  console.log("Dialing archaeologists...");
   await Promise.all(
-    archs.map(async (arch) => {
-      const connection = await sarco.archaeologist.dialArchaeologist(arch);
+    archaeologists.map(async (arch) => {
+      const connection = await sarco.archaeologist.dialArchaeologist(arch).catch((error) => {
+        console.error(`Failed to dial archaeologist ${arch.profile.archAddress}`);
+        throw error;
+      });
       arch.connection = connection;
     })
-  );
+  ).catch((error) => {
+    console.error("Failed to dial archaeologists");
+    throw error;
+  });
+  console.log(`Successfully connected to ${archaeologists.length} archaeologists`);
 
-  console.log("🚀 ~ file: index.ts:15 ~ main ~ archs:", archs);
+  // 4. Initiate sarcophagus negotiation
+  console.log("\n4. Initiate Sarcophagus Negotiation");
+  console.log("Initiating sarcophagus negotiation...");
+  // Retry up to 5 times
+  let responses = [];
+  let negotiationResult: Map<string, ArchaeologistNegotiationResult>;
+  let negotiationTimestamp: number;
+  for (let attempt = 0; attempt < negotiationAttemptCount; attempt++) {
+    try {
+      [negotiationResult, negotiationTimestamp] =
+        await sarco.archaeologist.initiateSarcophagusNegotiation(archaeologists);
+      responses = archaeologists.map((arch) => negotiationResult.get(arch.profile.peerId)!);
+      console.log("Negotiation successful");
+      break; // If successful, break the loop
+    } catch (error) {
+      console.error("Attempt " + (attempt + 1) + ": Failed to initiate sarcophagus negotiation");
+      // If we've reached the max attempts, re-throw the error
+      if (attempt === negotiationAttemptCount - 1) {
+        throw error;
+      }
+      // Otherwise, wait a bit before retrying
+      else {
+        await new Promise((resolve) => setTimeout(resolve, negotiationAttemptTimeout));
+      }
+    }
+  }
 
-  const [negotiationResult, negotiationTimestamp] =
-    await sarco.archaeologist.initiateSarcophagusNegotiation(archs);
+  // 5. Upload file to Arweave
+  console.log("\n5. Upload File to Arweave");
+  console.log("Uploading file to Arweave...");
+  function onStep(step: string) {}
+  function onUploadChunk(chunkedUploader: ChunkingUploader, chunkedUploadProgress: number) {}
+  function onUploadChunkError(msg: string) {}
 
-  const responses = archs.map((arch) => negotiationResult.get(arch.profile.peerId)!);
-  console.log("🚀 ~ file: index.ts:24 ~ main ~ responses:", responses);
+  let uploadId = "";
+  function onUploadComplete(_uploadId: string) {
+    uploadId = _uploadId;
+  }
 
   const archPublicKeys = responses.map((response) => response.publicKey);
-
-  //
-
-  const payloadWallet = ethers.Wallet.createRandom();
-  console.log("🚀 ~ file: index.ts:26 ~ main ~ payloadWallet:", payloadWallet);
-  const recipientWallet = ethers.Wallet.createRandom();
-  console.log("🚀 ~ file: index.ts:28 ~ main ~ recipientWallet:", recipientWallet);
-
-  const testData = "Hello World!";
-  const buffer = Buffer.from(testData, "utf-8");
-
-  function onUploadChunk(chunkedUploader: ChunkingUploader, chunkedUploadProgress: number) {
-    console.log("🚀 ~ file: index.ts:33 ~ main ~ chunkedUploadProgress:", chunkedUploadProgress);
-  }
-
-  function onUploadChunkError(msg: string) {
-    console.log("🚀 ~ file: index.ts:37 ~ main ~ msg:", msg);
-  }
-
-  function onUploadComplete(uploadId: string) {
-    console.log("Upload complete!");
-    console.log("🚀 ~ file: index.ts:41 ~ main ~ uploadId:", uploadId);
-  }
-
+  const buffer = Buffer.from(fileContents, "utf-8");
   const uploadArgs = {
     payloadData: {
-      name: "hello_world.txt",
+      name: fileName,
       type: "text/plain",
       data: buffer,
     },
-    onStep: (step: string) => {
-      console.log(step);
-    },
+    onStep,
     payloadPublicKey: payloadWallet.publicKey,
     payloadPrivateKey: payloadWallet.privateKey,
     recipientPublicKey: recipientWallet.publicKey,
-    shares: 5,
-    threshold: 3,
+    shares: archaeologistAddresses.length,
+    threshold: requiredArchaeologistCount,
     archaeologistPublicKeys: archPublicKeys,
     onUploadChunk,
     onUploadChunkError,
     onUploadComplete,
   };
-  console.log("🚀 ~ file: index.ts:47 ~ main ~ uploadArgs:", uploadArgs);
-  await sarco.api.uploadFileToArweave(uploadArgs);
+  await sarco.api.uploadFileToArweave(uploadArgs).catch((error) => {
+    console.error("Failed to upload file to Arweave");
+    throw error;
+  });
 
-  // uploadAndSetArweavePayload
-  // approveSarcoToken
-  // submitSarcophagus
+  // 6. Approve SARCO token (this approves max amount)
+  console.log("\n6. Approve SARCO Token");
+  console.log("Approving SARCO token...");
+  const approveAmount = BigNumber.from(ethers.constants.MaxUint256);
+  await sarco.token.approve(approveAmount).catch((error) => {
+    console.error("Failed to approve SARCO token");
+    throw error;
+  });
+  console.log("Successfully approved SARCO token");
+
+  // 7. Make contract call to create Sarcophagus
+  console.log("\n7. Create Sarcophagus");
+  console.log("Creating Sarcophagus...");
+  const recipientState = {
+    address: recipientWallet.address,
+    publicKey: recipientWallet.publicKey,
+    setByOption: null,
+  };
+  const selectedArchaeologists = archaeologists;
+  const requiredArchaeologists = requiredArchaeologistCount;
+
+  const archPublicKeysMap = new Map<string, string>();
+  archaeologists.forEach((arch, i) => {
+    archPublicKeysMap.set(arch.profile.archAddress, archPublicKeys[i]);
+  });
+
+  const archaeologistSignatures = new Map<string, string>();
+  archaeologists.forEach((arch, i) => {
+    archaeologistSignatures.set(arch.profile.archAddress, responses[i].signature);
+  });
+
+  const { submitSarcophagusArgs } = sarco.utils.formatSubmitSarcophagusArgs({
+    name: sarcophagusName,
+    recipientState,
+    resurrection: resurrectionTime,
+    selectedArchaeologists,
+    requiredArchaeologists,
+    negotiationTimestamp,
+    archaeologistPublicKeys: archPublicKeysMap,
+    archaeologistSignatures,
+    arweaveTxId: uploadId,
+  });
+  const tx = await sarco.api.createSarcophagus(...submitSarcophagusArgs).catch((error) => {
+    console.error("Failed to create Sarcophagus");
+    throw error;
+  });
+
+  console.log("Successfully created Sarcophagus!");
+  console.log("TX hash: ", tx.hash);
+
+  process.exit(0);
 }
 
 main();
